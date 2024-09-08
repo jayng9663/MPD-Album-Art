49c0d9c368ac01f7e6b4d795e2eb4eb09543fee0
Jay <70954997+jayng9663@users.noreply.github.com>
2024-09-07 23:43:22 -0700

    Improved code in album_bg.sh

--- album_bg.sh
+++ album_bg.sh
@@ -3,6 +3,7 @@
 ALBUM="/tmp/album_cover.png"
 ALBUM_SIZE="350"
 EMB_ALBUM="/tmp/album_cover_embedded.png"
+#BACKUP_ALBUM="$HOME/.ncmpcpp/backup_album.png"
 MUSIC_DIR="$HOME/Music/"
 
 file="$MUSIC_DIR$(mpc --format %file% current)"
@@ -10,27 +11,18 @@ album="${file%/*}"
 
 err=$(ffmpeg -loglevel 16 -y -i "${file}" -an -vcodec copy $EMB_ALBUM 2>&1)
 if [ "$err" != "" ]; then
-art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
+art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
 else
   art="$EMB_ALBUM"
 fi
-if [ "$art" = "" ]; then
-  art="$HOME/.ncmpcpp/Blank.png"
-fi
+
+[ -z "$art" ] && art="$BACKUP_ALBUM"
+[ -z "$art" ] && exit 1
 
 width=$(ffmpeg -i "$art" 2>&1 | grep 'Stream' | grep -oP '\d{2,}x\d{2,}' | head -n1 | cut -d'x' -f1)
 height=$(ffmpeg -i "$art" 2>&1 | grep 'Stream' | grep -oP '\d{2,}x\d{2,}' | head -n1 | cut -d'x' -f2)
 
-if [ "$width" -lt "$ALBUM_SIZE" ]; then
-  max_size_x="$width"
-else
-  max_size_x="$ALBUM_SIZE"
-fi
-
-if [ "$height" -lt "$ALBUM_SIZE" ]; then
-  max_size_y="$height"
-else
-  max_size_y="$ALBUM_SIZE"
-fi
+max_size_x=$(( width < ALBUM_SIZE ? width : ALBUM_SIZE ))
+max_size_y=$(( height < ALBUM_SIZE ? height : ALBUM_SIZE ))
 
 convert "$art" -resize "${max_size_x}x-1" -crop "${ALBUM_SIZE}x${ALBUM_SIZE}+0+0" -gravity center -background none -extent "${ALBUM_SIZE}x${ALBUM_SIZE}" "$ALBUM"
