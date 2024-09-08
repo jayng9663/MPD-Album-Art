1cd0cab34db32a262f9eb734d17791ee09d4cdeb
Jay <70954997+jayng9663@users.noreply.github.com>
2024-09-07 23:42:37 -0700

    Removed BACKUP_ALBUM

--- album.sh
+++ album.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 ALBUM="/tmp/album_cover.png"
-BACKUP_ALBUM="$HOME/.ncmpcpp/backup_album.png"
 
 function change_album {
   clear
   img2sixel "$ALBUM"
 }
 
-if [ ! -f "$ALBUM" ]; then
-  cp "$BACKUP_ALBUM" "$ALBUM"
-fi
 while inotifywait -q -q -e close_write "$ALBUM"; do
   change_album
 done
