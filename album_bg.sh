a7d0c617994f16ef100a4cd6221dc0ff6d010421
Jay <70954997+jayng9663@users.noreply.github.com>
2024-09-16 20:07:58 -0700

    Update album_bg.sh

--- album_bg.sh
+++ album_bg.sh
@@ -4,29 +4,47 @@ ALBUM="/tmp/album_cover.png"
 ALBUM_SIZE="350"
 EMB_ALBUM="/tmp/album_cover_embedded.png"
 #BACKUP_ALBUM="$HOME/.ncmpcpp/backup_album.png"
+MPC_CMD="mpc"
 MUSIC_DIR="$HOME/Music/"
+ONLINE_ALBUM="/tmp/online_album.png"
 
-file="$MUSIC_DIR$(mpc --format %file% current)"
+file="$MUSIC_DIR$($MPC_CMD --format %file% current)"
 album="${file%/*}"
+
+album_name=$($MPC_CMD --format '%album%' current)
+artist_name=$($MPC_CMD --format '%artist%' current)
+release_date=$($MPC_CMD --format '%date%' current)
+
+urlencode() {
+  echo -n "$1" | perl -MURI::Escape -ne 'print uri_escape($_)'
+}
+
+album_name=$(urlencode "$album_name")
+artist_name=$(urlencode "$artist_name")
+release_date=$(urlencode "$release_date")
 
 if [ ! -d "$album" ]; then
   exit 1
 fi
 
-if ! ffmpeg -loglevel error -y -i "${file}" -an -vcodec copy "$EMB_ALBUM" 2>/dev/null; then
+if ! ffmpeg -loglevel error -y -i "$file" -an -vcodec copy "$EMB_ALBUM" 2>/dev/null; then
   art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
 else
   art="$EMB_ALBUM"
 fi
 
-[ -z "$art" ] && art="$BACKUP_ALBUM"
-[ -z "$art" ] && exit 1
+if [ -z "$art" ]; then
+  art="$BACKUP_ALBUM"
 
-dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=noprint_wrappers=1:nokey=1 "$art")
-width=$(echo "$dimensions" | head -n1)
-height=$(echo "$dimensions" | tail -n1)
+  id=$(wget -qO- "https://musicbrainz.org/ws/2/release/?query=artist:${album_name}%20release:${artist_name}%20date:${release_date}&fmt=json" | jq -r '.releases[0].id')
 
-max_size_x=$(( width < ALBUM_SIZE ? width : ALBUM_SIZE ))
-max_size_y=$(( height < ALBUM_SIZE ? height : ALBUM_SIZE ))
+  if [ "$id" != "null" ]; then
+    cover_art_url="http://coverartarchive.org/release/$id/front"
+    wget -q "$cover_art_url" -O "$ONLINE_ALBUM"
+    art="$ONLINE_ALBUM"
+  else
+    exit 1
+  fi
+fi
 
-convert "$art" -resize "${max_size_x}x-1" -crop "${ALBUM_SIZE}x${ALBUM_SIZE}+0+0" -gravity center -background none -extent "${ALBUM_SIZE}x${ALBUM_SIZE}" "$ALBUM"
+convert "$art" -resize "${ALBUM_SIZE}x${ALBUM_SIZE}^" -gravity center -crop "${ALBUM_SIZE}x${ALBUM_SIZE}+0+0" +repage "$ALBUM"
