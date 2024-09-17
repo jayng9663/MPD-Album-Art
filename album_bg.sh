c6a7888b3cd169da0f7991b767730682dfdf3261
Jay <70954997+jayng9663@users.noreply.github.com>
2024-09-16 17:48:54 -0700

    Fixed crash and improved the code
    - Fixed no album & backup_album crash.
    - Changed ffmpeg to ffprobe for gripping the album dimensions, decrease two function call to one.

--- album_bg.sh
+++ album_bg.sh
@@ -9,9 +9,12 @@ MUSIC_DIR="$HOME/Music/"
 file="$MUSIC_DIR$(mpc --format %file% current)"
 album="${file%/*}"
 
-err=$(ffmpeg -loglevel 16 -y -i "${file}" -an -vcodec copy $EMB_ALBUM 2>&1)
-if [ "$err" != "" ]; then
-art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
+if [ ! -d "$album" ]; then
+  exit 1
+fi
+
+if ! ffmpeg -loglevel error -y -i "${file}" -an -vcodec copy "$EMB_ALBUM" 2>/dev/null; then
+  art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
 else
   art="$EMB_ALBUM"
 fi
@@ -19,8 +22,9 @@ fi
 [ -z "$art" ] && art="$BACKUP_ALBUM"
 [ -z "$art" ] && exit 1
 
-width=$(ffmpeg -i "$art" 2>&1 | grep 'Stream' | grep -oP '\d{2,}x\d{2,}' | head -n1 | cut -d'x' -f1)
-height=$(ffmpeg -i "$art" 2>&1 | grep 'Stream' | grep -oP '\d{2,}x\d{2,}' | head -n1 | cut -d'x' -f2)
+dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=noprint_wrappers=1:nokey=1 "$art")
+width=$(echo "$dimensions" | head -n1)
+height=$(echo "$dimensions" | tail -n1)
 
 max_size_x=$(( width < ALBUM_SIZE ? width : ALBUM_SIZE ))
 max_size_y=$(( height < ALBUM_SIZE ? height : ALBUM_SIZE ))
