398c47c50ce8365ca3a5de97bf37e5c3719547dd
jayng9663 <jayng9663@gmail.com>
2024-09-16 23:52:22 -0700

    Improved the code in album_bg.sh, run time faster.

--- album_bg.sh
+++ album_bg.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-MPC_CMD="mpc"
 ALBUM="/tmp/album_cover.png"
 ALBUM_SIZE="350"
 EMB_ALBUM="/tmp/album_cover_embedded.png"
+MPC_CMD="mpc"
 #BACKUP_ALBUM="$HOME/.ncmpcpp/backup_album.png"
 MUSIC_DIR="$HOME/Music/"
+
+#Download from MusicBrainz
 DOWNLOAD_FROM_INTERNET=1
 ONLINE_ALBUM="/tmp/online_album.png"
 
 file="$MUSIC_DIR$($MPC_CMD --format %file% current)"
 album="${file%/*}"
 
-album_name=$($MPC_CMD --format '%album%' current)
-artist_name=$($MPC_CMD --format '%artist%' current)
-release_date=$($MPC_CMD --format '%date%' current)
-
 urlencode() {
   echo -n "$1" | perl -MURI::Escape -ne 'print uri_escape($_)'
 }
 
-album_name=$(urlencode "$album_name")
-artist_name=$(urlencode "$artist_name")
-release_date=$(urlencode "$release_date")
-
-if [ ! -d "$album" ]; then
+if [ -z "$($MPC_CMD --format %file% current)" ] || [ ! -d "$album" ]; then
+  convert "$BACKUP_ALBUM" -resize "${ALBUM_SIZE}x${ALBUM_SIZE}^" -gravity center -crop "${ALBUM_SIZE}x${ALBUM_SIZE}+0+0" +repage "$ALBUM"
   exit 1
 fi
 
@@ -36,10 +31,14 @@ fi
 
 if [ -z "$art" ]; then
   art="$BACKUP_ALBUM"
-
-  if [ "$DOWNLOAD_FROM_INTERNET" = 1 ]; then
-    id=$(wget -qO- "https://musicbrainz.org/ws/2/release/?query=artist:${album_name}%20release:${artist_name}%20date:${release_date}&fmt=json" | jq -r '.releases[0].id')
-
+    if [ "$DOWNLOAD_FROM_INTERNET" -eq 1 ]; then
+    album_name=$(urlencode "$($MPC_CMD --format '%album%' current)")
+    artist_name=$(urlencode "$($MPC_CMD --format '%artist%' current)")
+    release_date=$(urlencode "$($MPC_CMD --format '%date%' current)")
+    if [ -z "$album_name" ] && [ -z "$artist_name" ]; then
+      exit 1
+    fi
+    id=$(wget -qO- "https://musicbrainz.org/ws/2/release/?query=artist:${artist_name}%20release:${album_name}%20date:${release_date}&fmt=json" | jq -r '.releases[0].id')
     if [ "$id" != "null" ]; then
       cover_art_url="http://coverartarchive.org/release/$id/front"
       wget -q "$cover_art_url" -O "$ONLINE_ALBUM"
@@ -50,4 +49,5 @@ if [ -z "$art" ]; then
   fi
 fi
 
-convert "$art" -resize "${ALBUM_SIZE}x${ALBUM_SIZE}^" -gravity center -crop "${ALBUM_SIZE}x${ALBUM_SIZE}+0+0" +repage "$ALBUM"
+convert "$art" -resize "${ALBUM_SIZE}x${ALBUM_SIZE}^" -gravity center -crop "${ALBUM_SIZE}x${ALBUM_SIZE}+0+0" +repage "$ALBUM" 2>/dev/null
+exit 0
