05eb859dbc27d49b2dca1140bb34c5f18304a987
Jay <70954997+jayng9663@users.noreply.github.com>
2024-09-17 03:00:08 +0000

    Revert "Fixed crash and improved the code"
    This reverts commit c6a7888b3cd169da0f7991b767730682dfdf3261.

--- album_bg.sh
+++ album_bg.sh
@@ -11,6 +11,7 @@ ONLINE_ALBUM="/tmp/online_album.png"
 file="$MUSIC_DIR$($MPC_CMD --format %file% current)"
 album="${file%/*}"
 
+<<<<<<< HEAD
 album_name=$($MPC_CMD --format '%album%' current)
 artist_name=$($MPC_CMD --format '%artist%' current)
 release_date=$($MPC_CMD --format '%date%' current)
@@ -29,6 +30,11 @@ fi
 
 if ! ffmpeg -loglevel error -y -i "$file" -an -vcodec copy "$EMB_ALBUM" 2>/dev/null; then
   art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
+=======
+err=$(ffmpeg -loglevel 16 -y -i "${file}" -an -vcodec copy $EMB_ALBUM 2>&1)
+if [ "$err" != "" ]; then
+art=$(find "$album" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | grep -i -m 1 -E "front|cover")
+>>>>>>> parent of c6a7888 (Fixed crash and improved the code)
 else
   art="$EMB_ALBUM"
 fi
@@ -36,7 +42,12 @@ fi
 if [ -z "$art" ]; then
   art="$BACKUP_ALBUM"
 
+<<<<<<< HEAD
   id=$(wget -qO- "https://musicbrainz.org/ws/2/release/?query=artist:${album_name}%20release:${artist_name}%20date:${release_date}&fmt=json" | jq -r '.releases[0].id')
+=======
+width=$(ffmpeg -i "$art" 2>&1 | grep 'Stream' | grep -oP '\d{2,}x\d{2,}' | head -n1 | cut -d'x' -f1)
+height=$(ffmpeg -i "$art" 2>&1 | grep 'Stream' | grep -oP '\d{2,}x\d{2,}' | head -n1 | cut -d'x' -f2)
+>>>>>>> parent of c6a7888 (Fixed crash and improved the code)
 
   if [ "$id" != "null" ]; then
     cover_art_url="http://coverartarchive.org/release/$id/front"
